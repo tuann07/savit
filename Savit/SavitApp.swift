879b18e1fa7c68fd30e417f1a767a56fd69b39c9
tuann07 <tuann07@users.noreply.github.com>
2022-07-30 15:23:12 +0700

    Initial Commit

---
+++ Savit/SavitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavitApp.swift
+//  Savit
+//
+//  Created by Tuan Nguyen Anh on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SavitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
